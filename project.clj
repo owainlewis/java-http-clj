764a57f7bc751dd4b70c95f75c8734ccb96568a7
Owain Lewis <owain.lewis@oracle.com>
2020-10-01 20:56:08 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.owainlewis/java-http-clj "0.2.0"
+(defproject com.owainlewis/java-http-clj "0.2.1-SNAPSHOT"
   :description "Clojure HTTP client based on the Java 11+ HTTP client"
   :url "http://github.com/owainlewis/java-http-clj"
   :license {:name "MIT"
