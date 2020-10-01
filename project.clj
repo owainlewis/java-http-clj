0e1aea4c53f2af6850682667733df538fdab8435
Owain Lewis <owain.lewis@oracle.com>
2020-10-01 18:05:41 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -11,5 +11,7 @@
   :deploy-repositories
   [["clojars"
     {:url "https://clojars.org/repo"
+     :username :env/clojars_username
+     :password :env/clojars_password
      :sign-releases false}]]
   :repl-options {:init-ns java-http-clj.core})
